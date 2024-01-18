c30d678fd173230ab2e6ff750597bbaced5b4dba
Kyusoo.Kim <anzel21@naver.com>
2024-01-19 00:28:28 +0900

    Base AudioWidget
    - make Base AudioWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:stellive_music/widget/audio_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -76,16 +78,8 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+    return const Scaffold(
+      backgroundColor: Colors.black,
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
@@ -105,21 +99,17 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            // const Text(
+            //   'You have pushed the button this many times:',
+            // ),
+            // Text(
+            //   '$_counter',
+            //   style: Theme.of(context).textTheme.headlineMedium,
+            // ),
+            AudioWidget(),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ lib/widget/audio_widget.dart
@@ -0,0 +1,342 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:youtube_explode_dart/youtube_explode_dart.dart' as yt;
+import 'package:youtube_explode_dart/youtube_explode_dart.dart';
+
+class AudioWidget extends StatefulWidget {
+  const AudioWidget({super.key});
+
+  @override
+  State<AudioWidget> createState() => _AudioWidgetState();
+}
+
+class _AudioWidgetState extends State<AudioWidget> {
+  String imgUrl =
+      'https://firebasestorage.googleapis.com/v0/b/new-ml-6c02d.appspot.com/o/lessonAssets%2Fcs3%2Fch7%2Fls4%2Fearth.jpeg?alt=media&token=b9ce6139-5e08-495d-b74f-9dfce09e86e2';
+  String url =
+      'https://files.freemusicarchive.org//storage-freemusicarchive-org//tracks//CAsMyXsiK0RkmsBG2K75J4wdewYDJElKJCe1tSQM.mp3';
+
+  // Declare AudioPlayer variable
+  late AudioPlayer player;
+  bool isPlaying = false;
+  double volume = 0.5;
+  bool isVolumeDisabled = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _initialize();
+  }
+
+  Future<String?> extractAudioUrl(String videoUrl) async {
+    var youtube = YoutubeExplode();
+    // var video = await youtube.videos.get(videoUrl);
+
+    var streamManifest =
+        await youtube.videos.streamsClient.getManifest(videoUrl);
+
+    var audioOnlyStreams = streamManifest.audioOnly;
+
+    var audioStream = audioOnlyStreams.withHighestBitrate();
+
+    return audioStream.url.toString();
+  }
+
+  void _initialize() async {
+    // Instantiate AudioPlayer class
+    player = AudioPlayer();
+
+    String? audioURL =
+        await extractAudioUrl('https://www.youtube.com/watch?v=XY4mPKSe1zE');
+
+    // Set the audio url
+    await player.setUrl(audioURL!);
+
+    // Set the initial volume
+    await player.setVolume(volume);
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    player.dispose();
+    super.dispose();
+  }
+
+  void _disableVolume() async {
+    // Set volume to 0
+    await player.setVolume(0);
+    setState(() {
+      if (volume > 0) {
+        isVolumeDisabled = true;
+      }
+    });
+  }
+
+  void _activateVolume() async {
+    // Set volume to previous value before it was 0
+    await player.setVolume(volume);
+    setState(() {
+      isVolumeDisabled = false;
+    });
+  }
+
+  IconData _getVolumeIcon() {
+    return (player.volume == 0) ? Icons.volume_off : Icons.volume_up_rounded;
+  }
+
+  void _playAudio() async {
+    setState(() {
+      isPlaying = true;
+    });
+
+    // Play the audio
+    await player.play();
+  }
+
+  void _pauseAudio() async {
+    setState(() {
+      isPlaying = false;
+    });
+
+    // Pause the audio
+    await player.pause();
+  }
+
+  IconData _getPlayPauseIcon() {
+    return (isPlaying) ? Icons.pause : Icons.play_arrow;
+  }
+
+  // Listen for current audio play position,
+  // Listen for buffer position,
+  // Listen for max audio length
+  Stream<PositionData> get _positionDataStream =>
+      Rx.combineLatest3<Duration, Duration, Duration?, PositionData>(
+          player.positionStream,
+          player.bufferedPositionStream,
+          player.durationStream,
+          (position, bufferedPosition, duration) => PositionData(
+              position, bufferedPosition, duration ?? Duration.zero));
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          height: 200,
+          width: 300,
+          child: Image.network(imgUrl),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              onPressed: () {
+                switch (isPlaying) {
+                  case true:
+                    return _pauseAudio();
+                  default:
+                    return _playAudio();
+                }
+              },
+              icon: Icon(_getPlayPauseIcon(), size: 50, color: Colors.white),
+            ),
+          ],
+        ),
+        const SizedBox(height: 16),
+        StreamBuilder<PositionData>(
+          stream: _positionDataStream,
+          builder: (context, snapshot) {
+            final positionData = snapshot.data;
+            Duration remaining = (positionData?.duration != null &&
+                    positionData?.position != null)
+                ? positionData!.duration - positionData.position
+                : Duration.zero;
+            return Row(
+              children: [
+                Expanded(
+                  child: SeekBar(
+                    duration: positionData?.duration ?? Duration.zero,
+                    position: positionData?.position ?? Duration.zero,
+                    bufferedPosition:
+                        positionData?.bufferedPosition ?? Duration.zero,
+                    onChangeEnd: player.seek,
+                  ),
+                ),
+                Text(
+                  RegExp(r'((^0*[1-9]d*:)?d{2}:d{2}).d+$')
+                          .firstMatch("$remaining")
+                          ?.group(1) ??
+                      '$remaining',
+                  style: Theme.of(context)
+                      .textTheme
+                      .caption
+                      ?.copyWith(color: Colors.white),
+                ),
+              ],
+            );
+          },
+        ),
+        const SizedBox(height: 16),
+        Row(
+          children: [
+            IconButton(
+              onPressed: () {
+                switch (isVolumeDisabled) {
+                  case true:
+                    return _activateVolume();
+                  default:
+                    return _disableVolume();
+                }
+              },
+              icon: Icon(_getVolumeIcon(), size: 30, color: Colors.white),
+            ),
+            Expanded(
+              child: Slider(
+                value: player.volume,
+                max: 1,
+                min: 0,
+                onChanged: (value) async {
+                  setState(() {
+                    volume = value;
+                  });
+                  await player.setVolume(value);
+                },
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+// The code below can be found in
+// just_audio github repository
+class SeekBar extends StatefulWidget {
+  final Duration duration;
+  final Duration position;
+  final Duration bufferedPosition;
+  final ValueChanged<Duration>? onChanged;
+  final ValueChanged<Duration>? onChangeEnd;
+
+  const SeekBar({
+    Key? key,
+    required this.duration,
+    required this.position,
+    required this.bufferedPosition,
+    this.onChanged,
+    this.onChangeEnd,
+  }) : super(key: key);
+
+  @override
+  SeekBarState createState() => SeekBarState();
+}
+
+class SeekBarState extends State<SeekBar> {
+  double? _dragValue;
+  late SliderThemeData _sliderThemeData;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    _sliderThemeData = SliderTheme.of(context).copyWith(
+      trackHeight: 2.0,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        SliderTheme(
+          data: _sliderThemeData.copyWith(
+            thumbShape: HiddenThumbComponentShape(),
+            activeTrackColor: Colors.blue.shade100,
+            inactiveTrackColor: Colors.grey.shade300,
+          ),
+          child: ExcludeSemantics(
+            child: Slider(
+              min: 0.0,
+              max: widget.duration.inMilliseconds.toDouble(),
+              value: min(widget.bufferedPosition.inMilliseconds.toDouble(),
+                  widget.duration.inMilliseconds.toDouble()),
+              onChanged: (value) {
+                setState(() {
+                  _dragValue = value;
+                });
+                if (widget.onChanged != null) {
+                  widget.onChanged!(Duration(milliseconds: value.round()));
+                }
+              },
+              onChangeEnd: (value) {
+                if (widget.onChangeEnd != null) {
+                  widget.onChangeEnd!(Duration(milliseconds: value.round()));
+                }
+                _dragValue = null;
+              },
+            ),
+          ),
+        ),
+        SliderTheme(
+          data: _sliderThemeData.copyWith(
+            inactiveTrackColor: Colors.transparent,
+          ),
+          child: Slider(
+            min: 0.0,
+            max: widget.duration.inMilliseconds.toDouble(),
+            value: min(_dragValue ?? widget.position.inMilliseconds.toDouble(),
+                widget.duration.inMilliseconds.toDouble()),
+            onChanged: (value) {
+              setState(() {
+                _dragValue = value;
+              });
+              if (widget.onChanged != null) {
+                widget.onChanged!(Duration(milliseconds: value.round()));
+              }
+            },
+            onChangeEnd: (value) {
+              if (widget.onChangeEnd != null) {
+                widget.onChangeEnd!(Duration(milliseconds: value.round()));
+              }
+              _dragValue = null;
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class HiddenThumbComponentShape extends SliderComponentShape {
+  @override
+  Size getPreferredSize(bool isEnabled, bool isDiscrete) => Size.zero;
+
+  @override
+  void paint(
+    PaintingContext context,
+    Offset center, {
+    required Animation<double> activationAnimation,
+    required Animation<double> enableAnimation,
+    required bool isDiscrete,
+    required TextPainter labelPainter,
+    required RenderBox parentBox,
+    required SliderThemeData sliderTheme,
+    required TextDirection textDirection,
+    required double value,
+    required double textScaleFactor,
+    required Size sizeWithOverflow,
+  }) {}
+}
+
+class PositionData {
+  final Duration position;
+  final Duration bufferedPosition;
+  final Duration duration;
+
+  PositionData(this.position, this.bufferedPosition, this.duration);
+}
