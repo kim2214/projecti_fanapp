2cd6b355acaa9ea67f685d8bc782ae00b23b4349
Kyusoo.Kim <anzel21@naver.com>
2025-01-29 02:14:46 +0900

    flutter_native_splash 도입
    - flutter_native_splash 패키지 도입
    - splash screen 베이스 작성

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:stellive_music/widget/audio_widget.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
 
