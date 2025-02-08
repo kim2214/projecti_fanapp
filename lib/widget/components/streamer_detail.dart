9a9127fdaea6e149c5ea88144894507f82369a32
Kyusoo.Kim <anzel21@naver.com>
2025-02-09 00:46:54 +0900

    streamer_detail 수정
    - streamer_detail base widget 사용 수정

--- lib/widget/components/streamer_detail.dart
+++ lib/widget/components/streamer_detail.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 
+import '../screen_base_widget.dart';
+
 class StreamerDetail extends StatelessWidget {
   const StreamerDetail({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Image.asset('assets/damyui_profile.png'),
-        SizedBox(
-          height: 20,
-        ),
-        Text('담유이'),
-      ],
+    return ScreenBaseWidget(
+      widget: Column(
+        children: [
+          Image.asset('assets/damyui_profile.png'),
+          SizedBox(
+            height: 20,
+          ),
+          Text('담유이'),
+        ],
+      ),
     );
   }
 }
